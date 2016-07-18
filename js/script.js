b5e68df4af8570b6e7971fdfe762c6160606b90a
Luca <ll698@cornell.edu>
2016-07-18 11:38:58 -0400

    memes

--- js/script.js
+++ js/script.js
@@ -7,21 +7,15 @@ $( document ).ready(function() {
     var scrollval = $(this).scrollTop();
         
     if (scrollval > 737) {
-        console.log(1);
         var nav = $(".nav");
         nav.removeClass("nav");
         nav.addClass("nav_fixed");
         
     }
     else {
-        console.log(2);
         var nav = $(".nav_fixed");
         nav.removeClass("nav_fixed");
         nav.addClass("nav");
     };
   });
 });
-
-
-
-$('.landing').parallax({imageSrc: '../assets/images/commons.jpg'});
