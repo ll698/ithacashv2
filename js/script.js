1a946527585dca607abe275b656c7b24233e9568
Lauren Hsu <Lauren Hsu>
2016-07-25 13:55:42 -0400

    Worked more on page responsiveness, still WIP

--- js/script.js
+++ js/script.js
@@ -2,29 +2,6 @@
 
 
 //DYNAMIC NAVBAR FUNCTIONALITY//
-/*
-//Luca's original code
-
-$( document ).ready(function() {
-    $(window).scroll(function() {
-    var scrollval = $(this).scrollTop();
-        
-    if (scrollval > 737) {
-        var nav = $(".nav");
-        nav.removeClass("nav");
-        nav.addClass("nav_fixed");
-        
-    }
-    else {
-        var nav = $(".nav_fixed");
-        nav.removeClass("nav_fixed");
-        nav.addClass("nav");
-    };
-  });
-});
-
-*/
-
 function NavCheck(threshold){
     $(window).scroll(function() {
     var scrollval = $(this).scrollTop();
@@ -41,11 +18,46 @@ function NavCheck(threshold){
         var nav = $(".nav_fixed");
         nav.removeClass("nav_fixed");
         nav.addClass("nav");
+        $('.nav').css('background', "#FFFFFF");
     };
   });
 }
 
 // media query change
+function WidthChange(mq) {
+  if (mq.matches) {
+    // window width is at least 500px
+    console.log("Computer");
+    NavCheck(737);
+  }
+  else {
+    // window width is less than 500px
+    //NEED TO PREVENT SECOND LISTENER FROM 
+    //CHANGING FROM COMPUTER TO TABLET INCORRECTLY
+    if (window.matchMedia("(min-width: 320px)").matches){
+        console.log("Tablet");
+        NavCheck(385);
+    }
+    else{
+        console.log("Mobile");
+        NavCheck(185); 
+    }
+  }
+}
+
+
+$( document ).ready(function() {
+    if (matchMedia) {
+    var mq = window.matchMedia("(min-width: 500px)");
+    //var mq2 = window.matchMedia("(min-width: 320px)");
+    mq.addListener(WidthChange);
+    //mq2.addListener(WidthChange2);
+    WidthChange(mq);
+    //WidthChange2(mq2);
+    } 
+});
+
+/*// media query change
 function WidthChange(mq) {
   if (mq.matches) {
     // window width is at least 750px
@@ -84,75 +96,19 @@ $( document ).ready(function() {
     WidthChange(mq);
     WidthChange2(mq2);
     } 
+});*/
+
+
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.parallax').css('margin-top', -(scrolled * 0.6) + 'px');
+    $('.bar-parallax').css('margin-top', -(-400 + (scrolled *  0.6)) + 'px');
+    $('.bar-parallax1').css('margin-bottom', -(-1200 + (scrolled *  0.6)) + 'px');
+    
+}
+
+$(window).scroll(function(e){
+    parallax();
 });
 
-/*
-function NavCheck(threshold){
-    $(window).scroll(function() {
-    var scrollval = $(this).scrollTop();
-
-    //console.log(threshold);
-        
-    if (scrollval > threshold) {
-        var nav = $(".nav");
-        nav.removeClass("nav");
-        nav.addClass("nav_fixed");
-        
-    }
-    else {
-        var nav = $(".nav_fixed");
-        nav.removeClass("nav_fixed");
-        nav.addClass("nav");
-    };
-  });
-}
-
-// media query change
-function WidthChange(mq) {
-  if (mq.matches) {
-    // window width is at least 750px
-    console.log("Computer");
-    NavCheck(737);
-  }
-  else {
-    // window width is less than 750px
-    //NEED TO PREVENT SECOND LISTENER FROM 
-    //CHANGING FROM COMPUTER TO TABLET INCORRECTLY
-    console.log("Tablet");
-    NavCheck(385);
-  }
-}
-
-function WidthChange2(mq2) {
-  if (mq2.matches) {
-    // window width is at least 320px
-    //NEED TO KEEP FOR RESIZING FROM MOBILE TO TABLET
-    console.log("Tablet");
-    NavCheck(385);
-  }
-  else{
-    console.log("Mobile");
-    NavCheck(185); 
-  }
-
-}
-
-$( document ).ready(function() {
-    if (matchMedia) {
-    var mq = window.matchMedia("(min-width: 750px)");
-    var mq2 = window.matchMedia("(min-width: 320px)");
-    mq.addListener(WidthChange);
-    mq2.addListener(WidthChange2);
-    WidthChange(mq);
-    WidthChange2(mq2);
-    } 
-});
-
-
-*/
-
-
-
-
-//SUBSECTION NAVBAR FUNCTIONALITY//
 
